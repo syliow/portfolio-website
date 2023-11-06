47eeb9026c66db56845045cd4213497dcd95b9ec
Shanyi <shanyiliow1107@gmail.com>
2023-11-06 23:34:18 +0800

    Updates

---
+++ precache-manifest.bb73505f83eaec73adcfeb407835229e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883734f562bc7f98b3d3a5ead14cff5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce69c9e498b07af6459",
+    "url": "/static/css/main.ab588325.chunk.css"
+  },
+  {
+    "revision": "3f3e7a930e0f4fd61f6c",
+    "url": "/static/js/2.35db43a5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.35db43a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce69c9e498b07af6459",
+    "url": "/static/js/main.5d070f22.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "a571ce1e40b892cf083404e52ac44c7f",
+    "url": "/static/media/LiowShanYi_Resume.a571ce1e.pdf"
+  },
+  {
+    "revision": "db3468c3cfdbde214e7a3ed5cf2ea86d",
+    "url": "/static/media/Untitled.db3468c3.png"
+  },
+  {
+    "revision": "12fc48ee28f5e56342106dd9fa3535c2",
+    "url": "/static/media/dmdasher.12fc48ee.png"
+  },
+  {
+    "revision": "4bf04bfe682599a116ad58e0ce401ee9",
+    "url": "/static/media/pengin-club.4bf04bfe.png"
+  },
+  {
+    "revision": "4c4a244aea45ac6a66d6f44e2c6441ee",
+    "url": "/static/media/sy_liow.4c4a244a.jpg"
+  }
+]);
